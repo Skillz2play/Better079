344bcf0ab8a2016a1bc427cafea1b3a315dd83f0
Skillz <jster723@gmail.com>
2021-08-20 14:15:42 -0400

    Add project files.

---
+++ Better079/Better079Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API;
+using Exiled.API.Features;
+using Exiled.Events;
+using Exiled;
+using Exiled.Events.EventArgs;
+
+namespace Better079
+{
+    public class Better079Plugin : Plugin<Config>
+    {
+        public static Better079Plugin instance;
+
+        public override string Name => "Better079";
+        public override string Author => "Skillz2play";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(2, 11, 0);
+        public PluginEvents PLEV;
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= PLEV.RoundStart;
+            Exiled.Events.Handlers.Player.Spawning -= PLEV.PlayerSpawn;
+            Exiled.Events.Handlers.Scp079.GainingExperience -= PLEV.ExpGain;
+            PLEV = null;
+            instance = null;
+        }
+
+        public override void OnEnabled()
+        {
+            instance = this;
+            PLEV = new PluginEvents(this);
+            Exiled.Events.Handlers.Server.RoundStarted += PLEV.RoundStart;
+            Exiled.Events.Handlers.Player.Spawning += PLEV.PlayerSpawn;
+            Exiled.Events.Handlers.Scp079.GainingExperience += PLEV.ExpGain;
+        }
+    }
+}
---
+++ Better079/PluginEvents.cs
@@ -0,0 +1,160 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Grenades;
+using Hints;
+using Interactables.Interobjects.DoorUtils;
+using MEC;
+using Mirror;
+using Respawning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Better079
+{
+    public class PluginEvents
+    {
+        private Better079Plugin plugin;
+        internal static float a2cooldown = 0f;
+
+        public PluginEvents(Better079Plugin better079Plugin)
+        {
+            this.plugin = better079Plugin;
+        }
+
+        internal void RoundStart()
+        {
+            a2cooldown = 0f;
+        }
+
+        internal void PlayerSpawn(SpawningEventArgs ev)
+        {
+            if (ev.Player.Role == RoleType.Scp079)
+            {
+                ev.Player.ReferenceHub.hints.Show(new TextHint(Better079Plugin.instance.Config.b079_spawn_msg, new HintParameter[] { new StringHintParameter("") }, new HintEffect[]
+                {
+                    HintEffectPresets.TrailingPulseAlpha(0.5f, 1f, 0.5f, 2f, 0f, 3)
+                }, 10f));
+                //ev.Player.Broadcast(10, plugin.SpawnMsg, false);
+            }
+        }
+
+        public static List<Camera079> GetSCPCameras()
+        {
+            var list = GameObject.FindObjectsOfType<Camera079>();
+            List<Camera079> cams = new List<Camera079>();
+            foreach (Exiled.API.Features.Player player in Exiled.API.Features.Player.List)
+            {
+                if (player.GameObject.GetComponent<ReferenceHub>().characterClassManager.CurRole.team == Team.SCP && player.GameObject.GetComponent<ReferenceHub>().characterClassManager.NetworkCurClass != RoleType.Scp079)
+                {
+                    foreach (var cam in list)
+                    {
+                        if (Vector3.Distance(cam.transform.position, player.CameraTransform.position) <= Better079Plugin.instance.Config.b079_a1_dist)
+                        {
+                            cams.Add(cam);
+                        }
+                    }
+                }
+            }
+            return cams;
+        }
+
+        public void ExpGain(GainingExperienceEventArgs ev)
+        {
+            if (plugin.Config.xpboost) return;
+
+            foreach (ExpGainType gainType in plugin.Config.experienceGain.Keys)
+                if (gainType == ev.GainType)
+                    ev.Amount += plugin.Config.experienceGain[gainType];
+        }
+
+        // https://github.com/galaxy119/EXILED/blob/master/EXILED_Main/Extensions/Player.cs
+        public static Room SCP079Room(ReferenceHub player)
+        {
+            Vector3 playerPos = player.scp079PlayerScript.currentCamera.transform.position;
+            Vector3 end = playerPos - new Vector3(0f, 10f, 0f);
+            bool flag = Physics.Linecast(playerPos, end, out RaycastHit raycastHit, -84058629);
+
+            if (!flag || raycastHit.transform == null)
+                return null;
+
+            Transform transform = raycastHit.transform;
+
+            while (transform.parent != null && transform.parent.parent != null)
+                transform = transform.parent;
+
+            foreach (Room room in Map.Rooms)
+                if (room.Position == transform.position)
+                    return room;
+            return null;
+            //return new Room(transform.name, transform, transform.position);
+        }
+
+        public static IEnumerator<float> GasRoom(Room room, ReferenceHub scp)
+        {
+            a2cooldown = Time.timeSinceLevelLoad;
+            if (!Better079Plugin.instance.Config.b079_a2_disable_cassie)
+            {
+                string str = ".g4 ";
+                for (int i = Better079Plugin.instance.Config.b079_a2_timer; i > 0f; i--)
+                {
+                    str += ". .g4 ";
+                }
+                RespawnEffectsController.PlayCassieAnnouncement(str, false, false);
+            }
+            List<DoorVariant> doors = Map.Doors.Where((d) => Vector3.Distance(d.transform.position, room.Position) <= 20f).ToList();
+            foreach (var item in doors)
+            {
+                item.NetworkTargetState = true;
+                item.ServerChangeLock(DoorLockReason.AdminCommand, true);
+            }
+            for (int i = Better079Plugin.instance.Config.b079_a2_timer; i > 0f; i--)
+            {
+                foreach (var ply in PlayerManager.players)
+                {
+                    var player = new Player(ply);
+                    if (player.Team != Team.SCP && player.CurrentRoom != null && player.CurrentRoom.Transform == room.Transform)
+                    {
+                        player.ReferenceHub.hints.Show(new TextHint(Better079Plugin.instance.Config.b079_msg_a2_warn.Replace("$seconds", "" + i), new HintParameter[] { new StringHintParameter("") }, new HintEffect[]
+                        {
+                            HintEffectPresets.TrailingPulseAlpha(0.5f, 1f, 0.5f, 2f, 0f, 3)
+                        }, 1f));
+                        //player.ClearBroadcasts();
+                        //player.Broadcast(1, plugin.A2WarnMsg.Replace("$seconds", "" + i), true);
+                        //PlayerManager.localPlayer.GetComponent<MTFRespawn>().RpcPlayCustomAnnouncement(".g3", false, false);
+                    }
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
+            foreach (var item in doors)
+            {
+                item.NetworkTargetState = false;
+                item.ServerChangeLock(DoorLockReason.AdminCommand, true);
+            }
+
+            for (int i = 0; i < Better079Plugin.instance.Config.b079_a2_gas_timer * 2; i++)
+            {
+                foreach (var ply in PlayerManager.players)
+                {
+                    var player = new Player(ply);
+                    if (player.Team != Team.SCP && player.Role != RoleType.Spectator && player.CurrentRoom != null && player.CurrentRoom.Transform == room.Transform)
+                    {
+                        player.ReferenceHub.playerStats.HurtPlayer(new PlayerStats.HitInfo(10f, "WORLD", DamageTypes.Decont, 0), player.ReferenceHub.gameObject);
+                        if (player.Role == RoleType.Spectator)
+                        {
+                            scp.scp079PlayerScript.AddExperience(Better079Plugin.instance.Config.b079_a2_exp);
+                        }
+                    }
+                }
+                yield return Timing.WaitForSeconds(0.5f);
+            }
+            foreach (var item in doors)
+            {
+                item.ServerChangeLock(DoorLockReason.AdminCommand, false);
+            }
+        }
+    }
+}
